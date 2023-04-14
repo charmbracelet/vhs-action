7415e595e4757758b12369661e53da77551c6ae0
Ayman Bagabas <ayman.bagabas@gmail.com>
2023-04-14 17:02:51 -0400

    feat(ci): add vhs from path test

--- src/main.ts
+++ src/main.ts
@@ -9,7 +9,8 @@ import * as exec from '@actions/exec'
 async function run(): Promise<void> {
   try {
     const version = core.getInput('version')
-    const filePath = core.getInput('path')
+    // set a default value to be backward compatible
+    const filePath = core.getInput('path') || 'vhs.tape'
 
     fs.accessSync(filePath, fs.constants.F_OK)
     fs.accessSync(filePath, fs.constants.R_OK)
@@ -17,11 +18,16 @@ async function run(): Promise<void> {
     await deps.install()
     const bin = await intaller.install(version)
 
-    core.debug('Adding VHS to PATH')
+    core.info('Adding VHS to PATH')
     core.addPath(path.dirname(bin))
 
-    if (path) {
+    // If the file exists, run it
+    // Otherwise, just install the binary
+    if (fs.existsSync(filePath)) {
+      core.info('Running VHS')
       await exec.exec(`${bin} ${filePath}`)
+    } else {
+      core.error(`File ${filePath} does not exist`)
     }
   } catch (error) {
     if (error instanceof Error) core.setFailed(error.message)
